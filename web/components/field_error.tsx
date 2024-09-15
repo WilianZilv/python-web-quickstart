26d66a8198cfbb23ec547e660be09676f88a09dd
Wilian Silva <wilianzilv@gmail.com>
2024-09-15 19:29:17 -0300

    feat(web/field_error): little animation so its naisu

--- web/components/field_error.tsx
+++ web/components/field_error.tsx
@@ -1,12 +1,14 @@
+import clsx from 'clsx';
 import { AlertCircle } from 'lucide-react'
 
 
 export function FieldError({ errors, path }: any) {
     const message = errors[path];
-    return message ? <div className='flex items-center gap-1'>
-        <AlertCircle size={12} className='text-red-500' />
-        <p className="text-xs text-red-500" > {message} </p>
+
+    const className = clsx('flex items-center gap-1 text-red-500 transition-all duration-500', message ? 'opacity-100 h-full' : 'opacity-0 h-0');
+    return <div className={className}>
+        <AlertCircle size={12} />
+        <p className="text-xs" > {message} </p>
     </div>
-        : null
 
 }
