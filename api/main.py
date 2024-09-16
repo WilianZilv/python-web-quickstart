1e9c426a88db29925efc9a17c45faf44852c373f
Wilian Silva <wilianzilv@gmail.com>
2024-09-16 01:32:03 -0300

    refactor(api/app): begins with api prefix to support vercel functions

--- api/main.py
+++ api/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI
 from api.helpers.exception_handler import http_exception_handler
 from api.routes import todos, users
 
 app = FastAPI()
 
+
 app.add_exception_handler(Exception, http_exception_handler)
 
-app.include_router(todos.router)
-app.include_router(users.router)
+api = APIRouter(prefix="/api")
+
+api.include_router(todos.router)
+api.include_router(users.router)
+
+app.include_router(api)
